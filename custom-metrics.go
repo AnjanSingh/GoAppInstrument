942104c079954c5fb77f9a6a0d75289737b68c48
AnjanSingh <anjanspeaks@gmail.com>
2022-03-22 17:31:40 +0530

    inital code for go custom metrics instrumentation

---
+++ custom-metrics.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		print("inside go func...")
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func main() {
+	print("hello...")
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
